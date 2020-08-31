dd8bdc2bef74032857a530e5433d1433c1c891ee
yoiber21 <yoiberrenteria2017@gmail.com>
2020-08-31 11:54:16 -0500

    commit

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Slider1 from "../slider1.jpg";
+import Slider2 from "../slider2.jpg";
+import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Inicio() {
   return (
     <div className="">
-      <h1 className="text-center mt-5">Inicio</h1>
+      <div className="row">
+        <hr />
+        <div className="col-md-6 m-5 mr-5 p-5">
+        
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../Logo.svg";
 import {Link} from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Navbar() {
   return (
     <div className="">
